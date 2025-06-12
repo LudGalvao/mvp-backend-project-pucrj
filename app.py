c7298a12967446d5b3b0b199a8b5cdbb4b1ed02e
LudGalvao <ludgalvao09@gmail.com>
2025-06-12 15:46:31 -0300

    Refatora app.py para incluir rotas de exercícios, histórico e contador, além de atualizar a descrição e versão da API.

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI
 from sqlalchemy import create_engine
 from models.base import Base
-from models.exercicios import Exercicios
-from models.exercicios_ativos import Exercicios_ativos
-from models.exercicios_historico import Exercicios_historicos
 from config.settings import SQLALCHEMY_DATABASE_URL, APP_NAME
+from routes import exercicios, exercicios_historico, contador, concluir_exercicios_ativos
 
 # Configuração do banco de dados
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
@@ -12,7 +10,16 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread
 # Criar as tabelas do banco de dados
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title=APP_NAME)
+app = FastAPI(
+    title="API de Exercícios",
+    description="Documentação da API de Exercícios usando FastAPI e Swagger",
+    version="1.0.0"
+)
+
+app.include_router(exercicios.router)
+app.include_router(exercicios_historico.router)
+app.include_router(contador.router)
+app.include_router(concluir_exercicios_ativos.router)
 
 @app.get("/")
 def read_root():
