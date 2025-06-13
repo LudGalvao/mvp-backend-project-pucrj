be3c123e5a097cd8942395468b00885c0b3e8984
LudGalvao <ludgalvao09@gmail.com>
2025-06-12 21:14:32 -0300

    Adiciona coluna 'exercicio_ativo' e índices à tabela Exercicios para otimizar consultas e gerenciar o estado dos exercícios.

--- models/exercicios.py
+++ models/exercicios.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Index, Boolean
 from models.base import Base
 
 class Exercicios(Base):
@@ -8,8 +8,16 @@ class Exercicios(Base):
     nome = Column(String(100), nullable=False)
     descricao = Column(String(500), nullable=False)
     vantagens = Column(String(500), nullable=False)
+    exercicio_ativo = Column(Boolean, nullable=False, default=False)
 
-    def __init__(self, nome:str, descricao:str, vantagens:str):
+    # Adicionando índices
+    __table_args__ = (
+        Index('idx_exercicios_nome', 'nome'),
+        Index('idx_exercicios_ativo', 'exercicio_ativo'),
+    )
+
+    def __init__(self, nome:str, descricao:str, vantagens:str, exercicio_ativo:bool = False):
         self.nome = nome
         self.descricao = descricao
         self.vantagens = vantagens
+        self.exercicio_ativo = exercicio_ativo
