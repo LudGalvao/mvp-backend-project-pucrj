acbe01097fa5dff5a070ac4a96551d8fb3195cef
LudGalvao <ludgalvao09@gmail.com>
2025-06-12 21:14:06 -0300

    Remove a configuração do banco de dados diretamente no app.py e importa a engine de um novo módulo de sessão. Também remove a rota raiz que retornava o nome do aplicativo.

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 from fastapi import FastAPI
-from sqlalchemy import create_engine
 from models.base import Base
-from config.settings import SQLALCHEMY_DATABASE_URL, APP_NAME
+from config.settings import APP_NAME
 from routes import exercicios, exercicios_historico, contador, concluir_exercicios_ativos
-
-# Configuração do banco de dados
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+from database.session import engine
 
 # Criar as tabelas do banco de dados
 Base.metadata.create_all(bind=engine)
@@ -20,7 +17,3 @@ app.include_router(exercicios.router)
 app.include_router(exercicios_historico.router)
 app.include_router(contador.router)
 app.include_router(concluir_exercicios_ativos.router)
-
-@app.get("/")
-def read_root():
-    return {"message": APP_NAME}
